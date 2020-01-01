32788985e99cf781c67d5ceab0fa61dccfbf02c7
fadimata tillman <fadi.tillman@gmail.com>
2020-01-01 06:14:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch =>
+      {},
+      :matriarch =>
+      {}
+      :hero=> {},#your key/value pairs here!
+      :hero_friends =>
+      []
+    }, 
     :capulet => {
-      #your key/value pairs here!
+      ;patriarch =>
+      {},
+      :matriarch =>#your key/value pairs here!
       }}
 
   
